9b57aa686ab26266e4d1e61cb0199632d6cbe96d
mw <304321003@qq.com>
2021-03-29 15:28:08 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/DockerController.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerController {
+    @GetMapping("/docker")
+    public String docker() {
+        return "Hello Docker";
+    }
+}
